21f9a40cc38013d2a2db8d6afcb861723542a45b
Vlad Denisov <vlad.a.denisov@gmail.com>
2020-05-14 17:27:41 +0300

    music: reactions and queue list is back

--- utils/api/reactions.js
+++ utils/api/reactions.js
@@ -8,21 +8,18 @@ const handleReaction = async (client, message, reaction) => {
   if (reaction.message.channel.name !== 'music_req') return
   if (!reaction.message.author.bot) return
   if (reaction.emoji.name === '⏭') {
-    server.dispatcher.end()
+    server.player.stop()
   }
   if (reaction.emoji.name === '⏹') {
     VOICE_API.leave(client, message)
   }
   if (reaction.emoji.name === '⏯') {
-    if (!server.dispatcher.isPaused) {
-      server.dispatcher.isPaused = true;
-      server.dispatcher.pause()
+    if (!server.player.paused) {
+      server.player.pause(true)
     } else {
-      server.dispatcher.isPaused = false
-      server.dispatcher.resume()
+      server.player.pause(false)
     }
   }
-
 }
 exports.hook = async (client, message) => {
   client.on('messageReactionAdd', async (reaction, _user) => {
