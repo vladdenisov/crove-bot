648c7eb8519fce9537e5e4b36e5470c574f16561
Vlad Denisov <vlad.a.denisov@gmail.com>
2020-05-14 16:41:19 +0300

    lavalink: initial queue funtional

--- utils/music/music.js
+++ utils/music/music.js
@@ -1,7 +1,8 @@
 const fetch = require('node-fetch')
+const ytdl = require('ytdl-core')
 
 const voice = require('../api/voice')
-const getSongs = async (server, search) => {
+exports.songs = async (server, search) => {
   const node = { host: 'localhost', port: 2333, password: 'youshallnotpass' }
 
   // eslint-disable-next-line node/no-unsupported-features/node-builtins
@@ -17,6 +18,9 @@ const getSongs = async (server, search) => {
     })
 }
 exports.run = async (client, message) => {
+  // eslint-disable-next-line security/detect-unsafe-regex
+  const URL_PATTERN = /^(?:\w+:)?\/\/(\S+)$/
+  const args = message.content.trim().split(/ +/g)
   message.delete()
   try {
     await voice.join(client, message)
@@ -25,8 +29,30 @@ exports.run = async (client, message) => {
     else console.error(e)
     return e
   }
-  const args = message.content.trim().split(/ +/g)
-  const [song] = await getSongs(servers[message.guild.id], `ytsearch: ${ args.join(' ') }`)
-  console.log(song)
-  await servers[message.guild.id].player.play(song.track)
+  let song = ''
+  const server = servers[message.guild.id]
+  if (!URL_PATTERN.test(message.content)) [song] = await this.songs(servers[message.guild.id], `ytsearch:${ args.join(' ') }`)
+  else [song] = await this.songs(servers[message.guild.id], `${ args.join(' ') }`)
+  if (!song) return 0
+  if (song.info.uri.includes('youtube')) {
+    const info = await ytdl.getBasicInfo(song.info.uri)
+    song.info.image = info.player_response.videoDetails.thumbnail.thumbnails[info.player_response.videoDetails.thumbnail.thumbnails.length - 1].url
+  }
+  server.queue.push(song)
+  if (!server.player.playing) await play(server)
+  console.log(server.queue)
+  return 0
+}
+
+const play = async server => {
+  await server.player.play(server.queue[0].track)
+  server.player.once('error', console.error)
+  server.player.once('end', data => {
+    if (data.reason === 'REPLACED') return
+    const shiffed = server.queue.shift()
+    if (server.loop === true) {
+      server.queue.push(shiffed)
+    }
+    play(server)
+  })
 }
