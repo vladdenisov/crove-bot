e8d0a6251453b58ea39a1cb1296af4e428380c42
Vlad Denisov <vlad.a.denisov@gmail.com>
2020-05-14 17:38:21 +0300

    seek: get it to work

--- commands/seek.js
+++ commands/seek.js
@@ -2,7 +2,12 @@
 exports.run = (client, message, args) => {
   if (!message.member.voice.channel) return
   if (!servers[message.guild.id]) return
-  if (!servers[message.guild.id].connection) return
+  if (!servers[message.guild.id].player) return
+  const server = servers[message.guild.id]
+  let time = args[0].split(':')
+  let ms = (+time[time.length - 1] + (+time[time.length - 2] * 60) + (time.length === 3 ? +time[0] * 3600 : 0)) * 1000
+  console.log(ms)
+  server.player.seek(ms)
 }
 
 exports.help = {
